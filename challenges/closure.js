1cb3da826d68f2919670d41e78a872ba675af425
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-09-12 13:58:57 -0400

    edited closure.js

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,24 +16,12 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: The nested function is a child of myFunction. Because it is defined inside the parent function, it has access to the parent function's scope. The child function can reach out to grab the parent's variables.
+// Explanation: The nested function is a child of myFunction. Because it is defined inside the parent function, it has access to the parent function's scope. The child function can reach out to grab the parent's variables and so can access internal.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
-
-// function summation(param){
-//   return counter(param);
-// }
-// function counter(value){
-//   let counter = 0;
-//   for (let i = 0; i <= value; i++ ){
-//     counter += i;
-//   }
-//   return counter;
-// }
-// console.log(summation(4));
 
 function summation(param){
   let sum = 0;
@@ -41,9 +29,9 @@ function summation(param){
     for (let i = 0; i <= param; i++ ){
       sum += i;
     }
-    return sum;   
+    return sum;
   }
   return counter();
 }
 console.log(summation(4));
-// console.log(summation(10));
+
